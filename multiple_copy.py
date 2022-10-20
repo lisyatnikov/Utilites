5ba07146adefad18ed129f769e93e13acfce1069
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-10-20 15:07:32 +0300

    added tree of files and folders FIXME: Exception - Trying to open a hiden file as a folder

--- multiple_copy.py
+++ multiple_copy.py
@@ -1,1 +1,19 @@
 import os
+
+os.chdir('c://2022-07-10 NRing')
+print(os.getcwd())
+print(os.listdir())
+print('\n')
+
+
+count = 0
+for dir in os.listdir():
+    print('folder ', count, ': ', dir)
+    os.chdir(dir)
+    print(os.listdir())
+    print('\n')
+    os.chdir('..')
+    count = count + 1
+count = None
+
+os.mkdir('c://destination_path')
