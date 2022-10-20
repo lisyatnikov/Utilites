04cf2b04c39f86ceca99902030c86395ae7a76d3
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-10-20 15:20:56 +0300

    added IF to verify that directory is a folder

--- multiple_copy.py
+++ multiple_copy.py
@@ -8,12 +8,14 @@ print('\n')
 
 count = 0
 for dir in os.listdir():
-    print('folder ', count, ': ', dir)
-    os.chdir(dir)
-    print(os.listdir())
-    print('\n')
-    os.chdir('..')
-    count = count + 1
+    # Checking that directory is a folder (not a hidden file .something)
+    if os.path.isdir(dir):
+        print('folder ', count, ': ', dir)
+        os.chdir(dir)
+        print(os.listdir())
+        print('\n')
+        os.chdir('..')
+        count = count + 1
 count = None
 
 os.mkdir('c://destination_path')
